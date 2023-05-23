831a8c9c9328190915ea3b0bbd5d2df72ed65b35
Joshua030 <Joseluy123>
2023-05-24 00:25:45 +0200

    Create a User.ts class component for creating and updating user data

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { User } from "./User";
+
+const user = new User({name:'myname', age: 20});
+
+user.set({name: 'newname', age: 8899});
+console.log( user.get('name'));
+console.log( user.get('age'));
