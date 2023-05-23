4aea1769bb34366aeb59154f58457c16e17c5f56
Joshua030 <Joseluy123>
2023-05-24 01:17:19 +0200

    Add event handler registration function

--- src/User.ts
+++ src/User.ts
@@ -1,9 +1,12 @@
 interface UserProps {
-  name:string; 
-  age: number;
+  name?:string; 
+  age?: number;
 }
 
+type Callback = () =>void;
+
 export class User {
+  events: {[key:string]:Callback[]}={}
   constructor(private data:UserProps) {}
 
   get(propName:string):(number | string){
@@ -11,7 +14,13 @@ return this.data[propName];
   }
 
   set(update:UserProps):void{
-    // Object.assign(this.data, update);
-    this.data = {...this.data, ...update};
+    Object.assign(this.data, update);
+    // this.data = {...this.data, ...update};
+  }
+  
+  on(eventName:string,callback:Callback): void{
+  const handlers = this.events[eventName] || [];
+  handlers.push(callback)
+  this.events[eventName] = handlers;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import { User } from "./User";
 
 const user = new User({name:'myname', age: 20});
 
-user.set({name: 'newname', age: 8899});
-console.log( user.get('name'));
-console.log( user.get('age'));
+user.on('change', ()=> {
+
+})
+user.on('change', ()=> {
+
+})
+user.on('prueba', ()=> {
+
+})
+
+console.log(user);
