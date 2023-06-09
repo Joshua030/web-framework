eb2f50969a6a8bb74b75aaae4b924919603de66f
Joshua030 <Joseluy123>
2023-06-09 19:33:44 +0200

    Feat, adding fetch functionality

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import { User } from "./User";
+import axios from 'axios';
 
-const user = new User({name:'myname', age: 20});
+// axios.post('http://localhost:3000/users', {
+//   name: 'myname',
+//   age: 20
+// });
 
-user.on('change', ()=> {
-
-})
-user.on('change', ()=> {
-
-})
-user.on('prueba', ()=> {
-
-})
-
-console.log(user);
+axios.get('http://localhost:3000/users/1');
