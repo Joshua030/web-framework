e8471892b11913facc365abde154020b20ce3305
Joshua030 <Joseluy123>
2023-06-10 21:43:19 +0200

    Refactor User, create a new classes Eventing and Sync

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import axios from 'axios';
+import { User } from "./models/User";
 
-// axios.post('http://localhost:3000/users', {
-//   name: 'myname',
-//   age: 20
-// });
 
-axios.get('http://localhost:3000/users/1');
+const user = new User ({name: 'new Record', age: 0});
+
+
+
+user.events.on('change', ()=>{
+  console.log('change!'); 
+})
+
+user.events.trigger('change');
