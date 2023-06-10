0874263c0dbc162b68176209f9a1ee988d0122d2
Joshua030 <Joseluy123>
2023-06-10 23:48:19 +0200

    Feat, Create attributes to  generate the differents field for each user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,6 @@
+import { Attributes } from "./Attributes";
 import Eventing from "./Eventing";
+import { Sync } from "./Sync";
 
 export interface UserProps {
   name?: string;
@@ -6,18 +8,14 @@ export interface UserProps {
   id?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users'
+
 export class User {
   public events: Eventing = new Eventing();
-  constructor(private data: UserProps) {}
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps> 
 
-  get(propName: string): number | string {
-    return this.data[propName];
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes <UserProps>(attrs)
   }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-    // this.data = {...this.data, ...update};
-  }
-
-  
 }
