de2dc4143252e8801073d2cda7290d29969e21be
Joshua030 <Joseluy123>
2023-07-05 21:52:26 +0200

    Last commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { User } from "./models/User";
+import { UserEdit } from "./views/UserEdit";
 import { UserForm } from "./views/UserForm";
 
 const user = User.buildUser({name: 'NAME', age: 20})
 
-const userForm = new UserForm(document.getElementById('root') as Element , user);
+// const userForm = new UserForm(document.getElementById('root') as Element , user);
 
-userForm.render();
+const root = document.getElementById('root');
+
+if(root) {
+  const userEdit = new UserEdit (root, user)
+  userEdit.render();
+  console.log(userEdit);
+  
+}
+else {
+throw new Error("rOOT eLEMENT NOT FOUND");
+}
+  
